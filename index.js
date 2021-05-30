274d813392f4c6637317951931a484ce9c2e0fc7
sky <flahertysky12@gmail.com>
2021-05-30 12:54:07 -0400

    completed assginment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+function superbowlWin(recordList) {
+    const win = recordList.find(record => {
+        return record.result === 'W' 
+     
+    }); 
+    if (win == undefined) {
+        return undefined 
+    }
+    return win.year;
+    
+}
+
+
+
+
+
+/*
+
+If win = undefined 
+
+
+
+
+compare w to something in object. 
+
+access key in object 
+
+
+what
+
+*/
